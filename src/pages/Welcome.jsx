b6b4b82548b2ce1af261062270847851dd66c81a
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2025-01-16 19:39:23 +0500

    first commit

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -237,22 +237,6 @@ const Welcome = () => {
                     </div>
                 </div>
             </div>
-            <div className='bg-[#FDFEFD] flex justify-center pt-36'>
-                <div className='w-[85%]'>
-                    <div className='flex justify-between'>
-                        <div className='w-[45%]'>
-                            <h2 className='text-[#0C231F] leading-none text-[48px] font-inter font-semibold'>Seamless trading and banking solutions.</h2>
-                            <p className='text-[#0c231F99] text-base font-medium font-inter mt-10'>Experience the ultimate financial revolution as you escrow your digital assets to buy stock and commodities options seamlessly.</p>
-                            <p className='text-[#0c231F99] text-base font-medium font-inter mt-5'>With our user-friendly platform, you can trade, swap, and sell tokenised shares with ease, all within a secure and transparent environment. Embrace the power of DeFi and diversify your digital investments to enjoy the potential gains from traditional stock markets. Join us now and unlock a world of limitless possibilities in the exciting realm of decentralised finance. </p>
-                            <p className='text-[#0c231F99] text-base font-medium font-inter mt-10'>Trusted by the world’s most ambitious companies</p>
-                            <img src={chain} className='w-[85%] mt-5' alt="" />
-                        </div>
-                        <div className='w-[45%]'>
-                            <img src={rightCard} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div className='flex justify-center mt-20'>
                 <div className='w-[85%]'>
                     <div className='w-[67%]'>
@@ -262,7 +246,6 @@ const Welcome = () => {
                 </div>
 
             </div>
-
             <div className='flex justify-center mt-10 tabs-sections'>
                 <div className='w-[85%]'>
                     <div className='flex justify-between items-center'>
@@ -289,109 +272,6 @@ const Welcome = () => {
                     </div>
                 </div>
             </div>
-
-            <div className='flex justify-center mt-20 tabs-sections'>
-                <div className='w-[85%]'>
-                    <div className='flex justify-between items-center'>
-                        <div className='shadow-lg w-[49%] pt-[60px] px-[80px] flex justify-center items-center rounded-2xl'
-                            style={{
-                                backgroundColor: 'transparent',
-                                backgroundImage: 'linear-gradient(180deg, #134341 0%, #277768 100%)',
-                            }}>
-                            <img src={personalWallet} className='rounded-t-2xl' />
-                        </div>
-                        <div className='w-[45%]'>
-                            <h2 className='text-[#0C231F] leading-none text-[48px] font-inter font-semibold'>Complete control over your wealth.</h2>
-                            <p className='text-[#0c231F99] text-justify text-base font-medium font-inter mt-4'>Unlike traditional financial systems where a third party, such as a bank, manages your funds, an online wallet allows you to be your own bank. You have the ability to send, receive and manage your Digital assets and fractional shares independently without relying on intermediaries.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className='flex justify-center mt-20 tabs-sections'>
-                <div className='w-[85%]'>
-                    <div className='flex justify-between items-center'>
-                        <div className='w-[45%]'>
-                            <h2 className='text-[#0C231F] leading-none text-[48px] font-inter font-semibold'>New and traditional financial synergy.</h2>
-                            <p className='text-[#0c231F99] text-justify text-base font-medium font-inter mt-4'>Fractional shares as a means of payment offers global accessibility, wealth diversification, potential appreciation and lower transaction costs. Additional benefits include capital gains tax optimisation, currency conversion saving and no external interventions to freeze personal wealth.</p>
-                        </div>
-                        <div className='shadow-lg w-[49%] pt-[60px] px-[80px] flex justify-center items-center rounded-2xl'
-                            style={{
-                                backgroundColor: 'transparent',
-                                backgroundImage: 'linear-gradient(180deg, #134341 0%, #277768 100%)',
-                            }}>
-                            <img src={currenies} className='rounded-t-2xl' />
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div className='flex justify-center mt-28 tabs-sections border-b-[4px] border-[#23645E]'>
-                <div className='w-[85%]'>
-                    <div className='flex gap-10 justify-between items-center'>
-                        <div className='w-[49%]'>
-                            <img src={cards} alt="" />
-                        </div>
-                        <div className='w-[49%]'>
-                            <h2 className='text-[48px] text-[#0C231F] font-semibold leading-none font-inter'>All-in-one decentralised digital app.</h2>
-                            <div className='mt-10 grid grid-cols-2 gap-5'>
-                                <div className='flex gap-2'>
-                                    <img src={check} className='w-3 h-3' alt="" />
-                                    <p className='text-base font-inter leading-none font-medium text-[#0C231F]'>Easy-to-Use Interface</p>
-                                </div>
-                                <div className='flex gap-2'>
-                                    <img src={check} className='w-3 h-3' alt="" />
-                                    <p className='text-base font-inter leading-none font-medium text-[#0C231F]'>Full Escrow Assurance</p>
-                                </div>
-                                <div className='flex gap-2'>
-                                    <img src={check} className='w-3 h-3' alt="" />
-                                    <p className='text-base font-inter leading-none font-medium text-[#0C231F]'>Digital to Equity Opions
-                                    </p>
-                                </div>
-                                <div className='flex gap-2'>
-                                    <img src={check} className='w-3 h-3' alt="" />
-                                    <p className='text-base font-inter leading-none font-medium text-[#0C231F]'>Community DOA</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
-            <div className='flex justify-center mt-28'>
-                <div className='w-[85%]'>
-                    <div className='flex gap-10 justify-between items-center'>
-                        <div className='w-[49%]'>
-                            <h2 className='text-[#0C231F] leading-none text-[48px] font-inter font-semibold'>About Us</h2>
-                            <p className='text-[#0c231F99] text-justify text-base font-medium font-inter mt-10'>Our innovative approach provides users with exposure to traditional stock markets without the need to convert their digital assets into fiat. By keeping their funds within the escrow contract, users avoid extra fees and complexities associated with transferring funds between different accounts. Additionally, this integration allows for seamless and instantaneous trading between tokenised shares and fiat, enabling users to capitalise on market opportunities swiftly. Moreover, users can diversify their investment portfolio effectively, balancing the potential gains from digital assets with the stability of traditional shares. Overall, this streamlined process ensures that users’ money never leaves the platform, enhancing security, convenience, and financial flexibility in one cohesive solution. </p>
-                        </div>
-                        <div className='w-[49%] flex items-center justify-center'>
-                            <img src={logo} className='w-[60%]' alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-
-            <div className='flex justify-center mt-28'>
-                <div className='w-[85%]'>
-                    <div className='flex gap-10 justify-between items-center'>
-                        <div className='w-[49%] flex items-center justify-center  bg-[#F1F3F3] py-5 px-14 rounded-2xl border-[3px] border-[#23645E]'>
-                            <img src={contract} alt="" />
-                        </div>
-                        <div className='w-[49%] sm:pl-10'>
-                            <h2 className='text-[#0C231F] leading-none text-[48px] font-inter font-semibold'>Smart contract automation</h2>
-                            <p className='text-[#0c231F99] text-justify text-base font-medium font-inter mt-5'>Smart contracts are at the heart of our groundbreaking approach to tokenised shares and secure transactions. Leveraging the power of smart contracts, we offer users the ability to trade and invest in tokenised shares seamlessly, without the need for traditional intermediaries. When users decide to lock their digital currency for tokenised shares, our smart contract escrow lock ensures that their funds are held securely until the predetermined conditions are met. </p>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
-
             <div className='flex justify-center mt-28'>
                 <div className='w-[85%]'>
                     <div className='flex gap-10 justify-between items-center'>
@@ -406,84 +286,8 @@ const Welcome = () => {
                     </div>
                 </div>
             </div>
-
-
-            <div className='flex justify-center mt-28'>
-                <div className='w-[85%]'>
-                    <div className='flex gap-10 justify-between items-center'>
-                        <div className='w-[45%] flex items-center justify-center  bg-[#F1F3F3] p-8 border-[3px] border-[#23645E]'>
-                            <img src={tradingInvesting} className='w-full' alt="" />
-                        </div>
-                        <div className='w-[54%] sm:pl-3'>
-                            <h2 className='text-[#0C231F] leading-none text-[48px] font-inter font-semibold'>Swap your currencies into equity in just one click.</h2>
-                            <p className='text-[#0c231F99] text-justify text-base font-medium font-inter mt-5'>Welcome to Open, where digital trading meets stock market. Our cutting-edge platform is designed to provide you with the best experience, with a wide range of features that cater to every investor’s needs.</p>
-                            <button className='hover:bg-[#E1FFA0] hover:text-[#35816C] bg-[#35816C] text-[#E1FFA0] transition px-8 py-3.5 rounded-lg mt-5 text-base font-semibold font-inter '>Learn More</button>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
-
-            <div className='flex justify-center mt-48'>
-                <div className='w-[85%] bg-[#134341] rounded-[20px] shadow-lg'>
-                    <div className='flex gap-10 justify-between items-start'>
-                        <div className='w-[35%] flex justify-center items-center min-h-[300px] relative'
-                            style={{
-                                backgroundColor: 'transparent',
-                                backgroundImage: 'radial-gradient(at top left, #E1FFA091 0%, #E1FFA000 70%)',
-                            }}>
-                            <img src={openLogo} className='w-[40%] z-10' alt="" />
-                            <div className='w-full h-full absolute left-0 top-0' style={{
-                                backgroundColor: 'transparent',
-                                backgroundImage: `url(${leftDots})`,
-                                backgroundSize: 'cover',
-                            }}></div>
-                        </div>
-                        <div className='w-[60%] pt-[70px] pr-[70px]'>
-                            <p className='font-inter text-lg font-semibold text-white'>
-                                "We believe that integrating tokenised fractional shares as a means of payment has the potential to revolutionise the financial landscape. We strive for financial inclusion, where anyone, regardless of their financial means, can leverage their fractional ownership to transact and thrive in a borderless and equitable world"
-                            </p>
-                            <h4 className='font-inter text-base text-[#E1FFA0] font-medium mt-5'>
-                                - Open Investment Team
-                            </h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div className='bg-[#F1F3F3] flex justify-center -mt-36 pb-[250px] pt-[350px]'>
-                <div className='w-[85%]'>
-                    <div className='flex justify-between items-start'>
-                        <div className='w-[40%] sm:pr-'>
-                            <img src={walletIntegrations} className='w-[13%]' alt="" />
-                            <h2 className='text-[#0C231F] leading-none text-[48px] font-inter font-semibold mt-5'>Wallet Integrations</h2>
-                            <p className='text-[rgba(12, 35, 31, 0.6)] text-base font-medium font-inter mt-5'>Wallet integration combines diverse digital assets into a single interface, offering users seamless management and control. With enhanced security measures and accessibility across devices, users can conveniently send, receive and participate in DeFi activities. The integration provides valuable insight into asset performance, empowering users to make informed decisions while ensuring their funds are protected.</p>
-                            <button className='hover:bg-[#E1FFA0] hover:text-[#35816C] bg-[#0C231F] text-[#E1FFA0] transition px-7 py-2.5 rounded-lg mt-5 text-base font-semibold font-inter '>Learn More</button>
-                        </div>
-                        <div className='w-[50%] grid grid-cols-2 gap-10'>
-                            <div>
-                                <img src={coinbase} className='w-[30%]' alt="" />
-                                <p className='text-[#0C231F] text-base font-medium font-inter mt-5'>Make secure payments online, and transfer digital currency with just a touch of your device.</p>
-                            </div>
-                            <div>
-                                <img src={metaMask} className='w-[30%]' alt="" />
-                                <p className='text-[#0C231F] text-base font-medium font-inter mt-5'>Easily link your personal wallet account to our platform for seamless and secure trading.</p>
-                            </div>
-                            <div>
-                                <img src={ledger} className='w-[30%]' alt="" />
-                                <p className='text-[#0C231F] text-base font-medium font-inter mt-5'>Accept payments from customers around the world directly, whilst storing your wealth offline and off-grid.</p>
-                            </div>
-                            <div>
-                                <img src={openWallet} className='w-[30%]' alt="" />
-                                <p className='text-[#0C231F] text-base font-medium font-inter mt-5'>Coming Soon! Open wallet with additional security features and escrow assurance on funds.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='bg-[#134341] -mt-[100px] rounded-t-[45px] py-[50px] relative'>
+           
+            <div className='bg-[#134341] mt-20 rounded-t-[45px] py-[50px] relative'>
                 <div className='opacity-[0.3] w-full h-full absolute top-0 left-0' style={{
                     backgroundColor: 'transparent',
                     backgroundImage: 'radial-gradient(at top center, #FFFFFF94 0%, #FFFFFF00 70%)',
